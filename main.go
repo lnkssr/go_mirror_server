47bbd79a0014f9363f368e519dddbf198b8cc186
lnkssr <gifzoner5@gmail.com>
2024-09-22 10:00:06 +0300

    add -log & logging

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
+	"os"
 )
 
 func mirrorConnection(src net.Conn, target string) {
@@ -11,40 +13,55 @@ func mirrorConnection(src net.Conn, target string) {
 
 	dst, err := net.Dial("tcp", target)
 	if err != nil {
-		log.Printf("Failed to connect to target: %v", err)
+		log.Printf("Failed to connect to target %s: %v", target, err)
 		return
 	}
 	defer dst.Close()
 
+	log.Printf("Connected to source %s and target %s", src.RemoteAddr(), dst.RemoteAddr())
+
 	go func() {
 		_, err := io.Copy(dst, src)
 		if err != nil {
-			log.Printf("Error copying from src to dst: %v", err)
+			log.Printf("Error copying from source %s to target %s: %v", src.RemoteAddr(), dst.RemoteAddr(), err)
 		}
 	}()
 	_, err = io.Copy(src, dst)
 	if err != nil {
-		log.Printf("Error copying from dst to src: %v", err)
+		log.Printf("Error copying from target %s to source %s: %v", dst.RemoteAddr(), src.RemoteAddr(), err)
 	}
 }
 
 func main() {
+	logFlag := flag.Bool("log", false, "Include/exclude logging to file")
+	flag.Parse()
+
 	adress_back := "localhost:8080"
 	adress_front := "0.0.0.0:8080"
 
+	if *logFlag {
+		f, _ := os.Create("main.log")
+		defer f.Close()
+		mw := io.MultiWriter(os.Stdout, f)
+		log.SetOutput(mw)
+	} else {
+		log.SetOutput(os.Stdout)
+	}
+
 	listener, err := net.Listen("tcp", adress_front)
 	if err != nil {
-		log.Fatalf("Failed to set up listener: %v", err)
+		log.Fatalf("Failed to set up listener on %s: %v", adress_front, err)
 	}
 	defer listener.Close()
-	log.Println("Listening on , mirroring to", adress_back)
+	log.Println("Listening on", adress_front, "mirroring to", adress_back)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
+			log.Printf("Error accepting connection: %v", err)
 			continue
 		}
+		log.Printf("Accepted connection from %s", conn.RemoteAddr())
 		go mirrorConnection(conn, adress_back)
 	}
 }
